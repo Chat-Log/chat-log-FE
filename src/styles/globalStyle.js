3ecb71c187433cd344faf2fae93246d4edcb5a9f
dkaodkaork <dkaodkaork@gmail.com>
2023-04-10 23:08:44 +0900

    [Style]: global style 세팅

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle` 
+    
+    ${reset} 
+
+    a{
+        text-decoration: none;
+        color: inherit;
+        cursor: pointer;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        /* font-size: 1.0rem; */
+        vertical-align: baseline;
+    }
+    input, textarea { 
+      -moz-user-select: auto;
+      -webkit-user-select: auto;
+      -ms-user-select: auto;
+      user-select: auto;
+    }
+    input:focus {
+      outline: none;
+    }
+
+    button {
+      border: none;
+      background: none;
+      padding: 0;
+      cursor: pointer;
+    }
+`;
+
+export default GlobalStyles;
