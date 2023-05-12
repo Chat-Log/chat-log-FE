ee17fb8dddae14a233e84e6d8226132861b4fb9b
dkaodkaork <dkaodkaork@gmail.com>
2023-05-13 05:03:01 +0900

    [Feat]: select

--- src/components/common/AntdSelect.jsx
+++ src/components/common/AntdSelect.jsx
@@ -2,35 +2,17 @@ import { Radio, Select } from "antd";
 import { useState } from "react";
 import styled from "styled-components";
 
-const AntdSelect = () => {
+const AntdSelect = ({ options, defaultValue, onChange }) => {
   const [placement, SetPlacement] = useState("bottomLeft");
 
   return (
     <>
-      <StSelect
-        defaultValue="Gpt3.5-turbo"
-        dropdownMatchSelectWidth={false}
-        placement={placement}
-        options={[
-          {
-            value: "Gpt3.5-turbo",
-            label: "Gpt3.5-turbo",
-          },
-          {
-            value: "Gpt4.0",
-            label: "Gpt4.0",
-          },
-          {
-            value: "Gpt3.5-davinchi",
-            label: "Gpt3.5-davinchi",
-          },
-        ]}
-      />
+      <StSelect defaultValue={defaultValue} dropdownMatchSelectWidth={false} placement={placement} options={options} onChange={onChange} />
     </>
   );
 };
 export default AntdSelect;
 
 const StSelect = styled(Select)`
-  width: 120px;
+  width: 170px;
 `;
