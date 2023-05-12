6fb41b01efb6eba570033a8c84ebd77fb7640a62
dkaodkaork <dkaodkaork@gmail.com>
2023-05-13 05:03:46 +0900

    [Feat]: answer card

---
+++ src/components/completion/Answer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+import { IMAGES } from "../../constants/index";
+import { reformatDate } from "../auth/func";
+import MarkdownRender from "./MarkdownRender";
+
+const Answer = ({ model, createdAt, answer }) => {
+  // console.log(answer);
+
+  return (
+    <StContainer>
+      <StProfile url={IMAGES.alborz} />
+      <StBody>
+        <StNickname>
+          {model} <p>{reformatDate(createdAt)}</p>
+        </StNickname>
+        <StContents>
+          <MarkdownRender markdown={answer} overflow="none" height="auto" />
+        </StContents>
+      </StBody>
+    </StContainer>
+  );
+};
+
+export default Answer;
+
+const StContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  background: ${({ bg }) => bg || "#E9F2FF"};
+  border: 1px solid rgba(0, 0, 0, 0.15);
+  min-height: 70px;
+
+  padding: 20px;
+
+  margin-bottom: 10px;
+`;
+const StProfile = styled.div`
+  width: 40px;
+  height: 40px;
+  background-image: url(${(props) => props.url});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  border-radius: 100%;
+`;
+const StBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-left: 20px;
+
+  line-height: 170%;
+  font-size: 1.2rem;
+`;
+
+const StNickname = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin: 3px;
+
+  p {
+    color: #2e2e2e;
+    opacity: 0.7;
+    margin-left: 10px;
+  }
+`;
+
+const StContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 5px;
+`;
