53dea1a769f979d46c8bbbc3376861b02df4ffb3
dkaodkaork <dkaodkaork@gmail.com>
2023-05-13 05:06:49 +0900

    [Feat]: sider

--- src/layout/AntdSider.jsx
+++ src/layout/AntdSider.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { PATH } from "../constants";
+import { useDispatch, useSelector } from "react-redux";
 
 import { FileOutlined, UserOutlined, SearchOutlined, QuestionOutlined, MenuUnfoldOutlined, MenuFoldOutlined, TagsOutlined } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
-const { Header, Content, Footer, Sider } = Layout;
+import { __getTopics } from "../redux/modules/mainSlice";
+const { Sider } = Layout;
 
 function getItem(label, key, icon, children) {
   return {
@@ -14,16 +18,73 @@ function getItem(label, key, icon, children) {
   };
 }
 
-const items = [
-  getItem("질문하기", "1", <QuestionOutlined />),
-  getItem("내역", "sub1", <FileOutlined />, [getItem("운영체제가뭐야?", "5"), getItem("네트워크가뭐야?", "6"), getItem("안녕?", "7")]),
-  getItem("마이페이지", "2", <UserOutlined />),
-  getItem("태그별 보기", "3", <TagsOutlined />),
-  getItem("검색", "4", <SearchOutlined />),
-];
+// [getItem("운영체제가뭐야?", "5"), getItem("네트워크가뭐야?", "6"), getItem("안녕?", "7")]
 
 const AntdSider = () => {
+  const dispatch = useDispatch();
+  const titleData = useSelector((state) => state.main?.titleData);
+  const navigate = useNavigate();
+
   const [collapsed, setCollapsed] = useState(false);
+  const param = useParams();
+  const topicId = param.topicId;
+
+  // const b = [getItem("운영체제가뭐야?", "5"), getItem("네트워크가뭐야?", "6"), getItem("안녕?", "7")];
+  //   const a = titles?.map((title, index) => getItem(title, `${index + 5}`));
+  //   console.log(a);
+
+  const titles = titleData?.map((item) => getItem(item?.title, item?.id));
+
+  useEffect(() => {
+    dispatch(__getTopics({ pagesize: "100", pageindex: "1" }));
+  }, [topicId]);
+
+  const items = [
+    getItem(
+      "질문하기",
+      "0",
+      <Link to={PATH.main}>
+        <QuestionOutlined />
+      </Link>
+    ),
+    getItem(
+      "내역",
+      "1",
+      // <Link to={PATH.search}>
+      <FileOutlined />,
+      // </Link>,
+      titles
+      // [getItem("운영체제가뭐야?", "5"), getItem("네트워크가뭐야?", "6"), getItem("안녕?", "7")]
+    ),
+    getItem(
+      "마이페이지",
+      "2",
+      <Link to={PATH.mypage}>
+        <UserOutlined />
+      </Link>
+    ),
+    getItem(
+      "태그별 보기",
+      "3",
+      <Link to={PATH.tag}>
+        <TagsOutlined />
+      </Link>
+    ),
+    getItem(
+      "검색",
+      "4",
+      <Link to={PATH.search}>
+        <SearchOutlined />
+      </Link>
+    ),
+  ];
+
+  const menuClickHandler = (e) => {
+    if (e.key.length > 2) {
+      navigate(PATH.main + "/" + e.key);
+    }
+  };
+
   const {
     token: { colorBgContainer },
   } = theme.useToken();
@@ -37,18 +98,20 @@ const AntdSider = () => {
       style={{
         background: colorBgContainer,
         overflow: "auto",
-        height: "100vh",
+        // height: "100vh",
+        marginRight: "5px",
       }}
     >
       <Menu
         mode="inline"
         defaultSelectedKeys={["1"]}
-        // defaultOpenKeys={["sub1"]} // 처음에 열리는거
+        // 처음에 열리는거
         style={{
-          height: "100%",
           borderRight: 0,
+          width: collapsed ? "75px" : "200px",
         }}
         items={items}
+        onClick={menuClickHandler}
       />
       <Button style={{ position: "fixed", bottom: "10px", left: "10px", border: "none", background: "transparent" }} size="large" shape="circle" onClick={() => setCollapsed((prev) => !prev)}>
         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
