de3a375ceb7a004ff23d7743be46156e051c1dc5
dkaodkaork <dkaodkaork@gmail.com>
2023-05-15 01:52:07 +0900

    [Feat]: 내역 수정 삭제  작업중

--- src/layout/AntdSider.jsx
+++ src/layout/AntdSider.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { PATH } from "../constants";
 import { useDispatch, useSelector } from "react-redux";
 
-import { FileOutlined, UserOutlined, SearchOutlined, QuestionOutlined, MenuUnfoldOutlined, MenuFoldOutlined, TagsOutlined } from "@ant-design/icons";
+import { EditOutlined, DeleteOutlined, FileOutlined, UserOutlined, SearchOutlined, QuestionOutlined, MenuUnfoldOutlined, MenuFoldOutlined, TagsOutlined } from "@ant-design/icons";
 import { Button, Layout, Menu, theme } from "antd";
 import { __getTopics } from "../redux/modules/mainSlice";
 const { Sider } = Layout;
@@ -29,14 +29,10 @@ const AntdSider = () => {
   const param = useParams();
   const topicId = param.topicId;
 
-  // const b = [getItem("운영체제가뭐야?", "5"), getItem("네트워크가뭐야?", "6"), getItem("안녕?", "7")];
-  //   const a = titles?.map((title, index) => getItem(title, `${index + 5}`));
-  //   console.log(a);
-
   const titles = titleData?.map((item) => getItem(item?.title, item?.id));
 
   useEffect(() => {
-    dispatch(__getTopics({ pagesize: "100", pageindex: "1" }));
+    dispatch(__getTopics({ pagesize: "10", pageindex: "1" }));
   }, [topicId]);
 
   const items = [
@@ -98,7 +94,7 @@ const AntdSider = () => {
       style={{
         background: colorBgContainer,
         overflow: "auto",
-        // height: "100vh",
+        height: "100vh",
         marginRight: "5px",
       }}
     >
