e8b6a50b39cfb8395d23afa85972f29070d1e32c
dkaodkaork <dkaodkaork@gmail.com>
2023-05-26 13:18:55 +0900

    [Style]: style 변경

--- src/layout/AntdLayout.jsx
+++ src/layout/AntdLayout.jsx
@@ -1,4 +1,4 @@
-import { Layout } from "antd";
+import { Layout, FloatButton } from "antd";
 import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import AntdHeader from "./AntdHeader";
@@ -6,18 +6,31 @@ import AntdSider from "./AntdSider";
 
 const AntdLayout = () => {
   return (
-    <Layout>
+    <StFullHeightLayout>
       <AntdHeader />
-      <Layout>
+      <StContentLayout>
+        {/* <StContainer> */}
         <AntdSider />
-        <StContainer>
-          <Outlet />
-        </StContainer>
-      </Layout>
-    </Layout>
+        <Outlet />
+        {/* </StContainer> */}
+      </StContentLayout>
+      <FloatButton.BackTop />
+    </StFullHeightLayout>
   );
 };
 
 export default AntdLayout;
 
 const StContainer = styled(Layout)``;
+
+const StFullHeightLayout = styled(Layout)``;
+
+const StContentLayout = styled(Layout)`
+  padding-top: 70px;
+  /* height: 100%; */
+  /* height: 50vh; */
+  /* overflow: hidden; */
+  /* height: 100%; */
+
+  /* overflow: hidden; */
+`;
