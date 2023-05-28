935eaf183086af3eaa00701bdf960dd1c4e11712
dkaodkaork <dkaodkaork@gmail.com>
2023-05-28 21:01:06 +0900

    [Style]: css 수정

--- src/components/common/CustomRadio.jsx
+++ src/components/common/CustomRadio.jsx
@@ -1,11 +1,29 @@
 import { Radio } from "antd";
+import styled from "styled-components";
 
 export const CustomRadio = ({ onChange, value, option }) => {
   return (
     <Radio.Group onChange={onChange} value={value}>
-      <Radio value={option.option1}>{option.옵션1}</Radio>
-      <Radio value={option.option2}>{option.옵션2}</Radio>
-      <Radio value={option.option3}>{option.옵션3}</Radio>
+      <StCustomRadio value={option.option1}>{option.옵션1}</StCustomRadio>
+      <StCustomRadio value={option.option2}>{option.옵션2}</StCustomRadio>
+      <StCustomRadio value={option.option3}>{option.옵션3}</StCustomRadio>
     </Radio.Group>
   );
 };
+
+const StCustomRadio = styled(Radio)`
+  &:hover .ant-radio-inner,
+  .ant-radio-input:focus + .ant-radio-inner {
+    border-color: #4ea4f4;
+  }
+  .ant-radio-checked::after {
+    border-color: #4ea4f4;
+  }
+  .ant-radio-checked .ant-radio-inner {
+    border-color: #4ea4f4;
+    background-color: #4ea4f4;
+  }
+  .ant-radio-inner::after {
+    background-color: white;
+  }
+`;
